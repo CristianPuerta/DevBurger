d5ea7fb659eb79afc35317e74fd1793110611d8a
Cristian <christian.naik@gmail.com>
2024-08-05 17:02:15 -0300

    Atualização migrate

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 
 import express from 'express'
-import routes from '.routes'
+import routes from './routes'
 
 
 class App {
     constructor() {
-        this.app = express()
-
+        this.app = express();
         this.middlewares();
         this.routes();
     }
